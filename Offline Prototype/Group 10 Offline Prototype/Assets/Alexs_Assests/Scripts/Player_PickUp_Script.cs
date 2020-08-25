3b0df59320b63b872d83ea4a6638b4fc9451014d
APCTophat <49924900+APCTophat@users.noreply.github.com>
2020-08-25 12:44:58 +0200

    Version 0.1f of the mechanics
    The bottle collection is exactly right but I will keep working on it. I just wanted to show the movement and the feel of the game.
    
    Let me know what you think

---
+++ Offline Prototype/Group 10 Offline Prototype/Assets/Alexs_Assests/Scripts/Player_PickUp_Script.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Alex.Carvalho.NameSpace
+{
+    public class Player_PickUp_Script : MonoBehaviour
+    {
+
+        #region public fields
+        public string bottleTag;
+
+        public float bottlesCollected;
+        #endregion
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void OnTriggerStay(Collider other)
+        {
+            if(other.gameObject.tag == bottleTag && Input.GetKeyDown(KeyCode.Space))
+            {
+                Destroy(other.gameObject);
+
+                bottlesCollected += 1;
+            }
+        }
+
+        public void OnTriggerEnter(Collider other)
+        {
+            if(other.gameObject.tag == bottleTag)
+            {
+                Debug.Log("Player_PickUp_Script : OntriggerEnter Collided with Bottle");
+            }
+        }
+    }
+}
