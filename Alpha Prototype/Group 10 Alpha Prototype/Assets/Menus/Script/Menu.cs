7b3899f0654a0a84067404e6a2f28364fe625e5c
Killaroach25 <64203081+Killaroach25@users.noreply.github.com>
2020-10-08 12:56:05 +0200

    UI

---
+++ Alpha Prototype/Group 10 Alpha Prototype/Assets/Menus/Script/Menu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Guide()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
---
+++ Alpha Prototype/Group 10 Alpha Prototype/Assets/Menus/Script/Pause.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public static bool isPause;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPause)
+            {
+                Resume();
+            }
+            else
+            {
+                pauseGame();
+            }
+        }
+    }
+
+    public void pauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPause = true;
+
+    }
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPause = false;
+
+    }
+
+    public void GoToMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(1);
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+
+
+
+}
